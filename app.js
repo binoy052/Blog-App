8bbddb04374907b675f6ff2f8a320fd724bcb6ec
binoy052 <hembrambinoy5@gmail.com>
2022-08-03 09:45:17 +0530

    Initial update

---
+++ app.js
@@ -0,0 +1,81 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const homeStartingContent = "Post your blog by going /compose url.";
+const aboutContent = "My Name is Binoy";
+const contactContent = "Contact me";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const DB_HOST = process.env.DB_HOST;
+const DB_PASS = process.env.DB_PASS;
+
+mongoose.connect(
+  `mongodb+srv://${DB_HOST}:${DB_PASS}@cluster0.q4z5o.mongodb.net/?retryWrites=true&w=majority/blogDB`,
+  { useNewUrlParser: true }
+);
+
+const postSchema = {
+  title: String,
+  content: String,
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function (req, res) {
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts,
+    });
+  });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  });
+
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content,
+    });
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
